f8a7363ccd1e23491e47abcda56207a287ff9436
JoyCrafter <imkoreanjoy@outlook.com>
2025-08-02 22:36:28 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-MEMORY=${MEMORY:-512M}
+MEMORY=${MEMORY:-1G}
 
 echo "Starting server with ${MEMORY} RAM"
 
-sshx &
+# SSH 서버를 백그라운드에서 실행합니다.
+/usr/sbin/sshd &
 
 exec java -Xmx${MEMORY} -Xms${MEMORY} -jar paper.jar nogui
